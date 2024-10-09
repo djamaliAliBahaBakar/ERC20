06857f34860dc6e50e35831fbd70856830f9d17c
Djamali Ali bakar <dalibakar@gmail.com>
2024-10-09 23:15:28 +0200

    More tests

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -7,4 +7,18 @@ contract OurToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Our Token", "TO") {
         _mint(msg.sender, initialSupply);
     }
+
+        function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, allowance(_msgSender(), spender) + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        uint256 currentAllowance = allowance(_msgSender(), spender);
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+        unchecked {
+            _approve(_msgSender(), spender, currentAllowance - subtractedValue);
+        }
+        return true;
+    }
 }
--- test/OurTokenTest.t.sol
+++ test/OurTokenTest.t.sol
@@ -1,43 +1,97 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+pragma solidity ^0.8.19;
+
 import {DeployOurToken} from "../script/DeployOurToken.s.sol";
-import {OurToken} from "src/OurToken.sol";
+import {OurToken} from "../src/OurToken.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
 
-contract OurTokenTest is Test{
+
+interface MintableToken {
+    function mint(address, uint256) external;
+}
+
+contract OurTokenTest is StdCheats, Test {
     OurToken public ourToken;
     DeployOurToken public deployer;
-    address bob = makeAddr("bob");
-    address alice = makeAddr("alice");
+    address public user1 = address(0x123);
+    address public user2 = address(0x456);
 
-    uint256 public constant STARTING_BALANCE = 100 ether;
+    event Transfer(address from, address to, uint256 value);
+    event Approval(address from, address to, uint256 value);
 
     function setUp() public {
         deployer = new DeployOurToken();
-        ourToken=  deployer.run();
+        ourToken = deployer.run();
+        vm.deal(user1, 1 ether); // Giving some initial balance to user1 for transactions
+        vm.deal(user2, 1 ether); // Giving some initial balance to user2 for transactions
+    }
 
+    function testInitialSupply() public view {
+        assertEq(ourToken.totalSupply(), deployer.INITIAL_SUPPLY());
+    }
+
+    function testUsersCantMint() public {
+        vm.expectRevert();
+        MintableToken(address(ourToken)).mint(address(this), 1);
+    }
+
+    function testAllowance() public {
+        uint256 amount = 1000 * 10 ** 18; // Example amount
         vm.prank(msg.sender);
-        ourToken.transfer(bob, STARTING_BALANCE);
+        ourToken.approve(user1, amount);
+
+        assertEq(ourToken.allowance(msg.sender, user1), amount);
     }
 
-    function testBobBalance() view public {
-        assertEq(STARTING_BALANCE, ourToken.balanceOf(bob));
-        assertEq(0, ourToken.balanceOf(msg.sender));
+    function testTransfer() public {
+        uint256 amount = 1000 * 10 ** 18; // Example amount
+        vm.prank(msg.sender);
+        ourToken.transfer(user1, amount);
+
+        assertEq(ourToken.balanceOf(user1), amount);
+        assertEq(ourToken.balanceOf(msg.sender), deployer.INITIAL_SUPPLY() - amount);
     }
 
-    function testAllowances() public {
-        uint256 initialAllowance= 1000;
-        vm.prank(bob);
-        ourToken.approve(alice, initialAllowance);
+    function testTransferFrom() public {
+        uint256 amount = 500 * 10 ** 18; // Example amount
+        vm.prank(msg.sender);
+        ourToken.approve(user1, amount);
 
-        uint256 transferAmount = 500;
+        vm.prank(user1);
+        ourToken.transferFrom(msg.sender, user2, amount);
 
-        vm.prank(alice);
-        ourToken.transferFrom(bob, alice, transferAmount);
-        assertEq(ourToken.balanceOf(alice), transferAmount);
-        assertEq(ourToken.balanceOf(bob), STARTING_BALANCE - transferAmount);
-
+        assertEq(ourToken.balanceOf(user2), amount);
+        assertEq(ourToken.allowance(msg.sender, user1), 0);
     }
 
-} 
+    function testFailTransferExceedsBalance() public {
+        uint256 amount = deployer.INITIAL_SUPPLY() + 1;
+        vm.prank(msg.sender);
+        ourToken.transfer(user1, amount); // This should fail
+    }
+
+    function testFailApproveExceedsBalance() public {
+        uint256 amount = deployer.INITIAL_SUPPLY() + 1;
+        vm.prank(msg.sender);
+        vm.expectRevert();
+        ourToken.approve(user1, amount); // This should fail
+    }
+
+    /*function testTransferEvent() public {
+        uint256 amount = 1000 * 10 ** 18; // Example amount
+        vm.prank(msg.sender);
+        vm.expectEmit(true, true, false, true);
+        emit Transfer(msg.sender, user1, amount);
+        ourToken.transfer(user1, amount);
+    }
+
+    function testApprovalEvent() public {
+        uint256 amount = 1000 * 10 ** 18; // Example amount
+        vm.prank(msg.sender);
+        vm.expectEmit(true, true, false, true);
+        emit Approval(msg.sender, user1, amount);
+        ourToken.approve(user1, amount);
+    }*/
+}
